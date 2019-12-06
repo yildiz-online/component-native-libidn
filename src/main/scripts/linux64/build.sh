c57e2abeb9157f2ea53b53b3ac298066929f1032
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-06 20:12:45 +0100

    [WIP] Build scripts.

---
+++ src/main/scripts/linux64/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#cp -R ../../c++ workdir
+#cd workdir
+#make
+#r1=$?
+#mkdir -p ../../../../../target/classes/linux64/include
+#mkdir -p ../../../../../target/classes/linux64/lib
+#cp Dist/FreeImage.h ../../../../../target/classes/linux64/include
+#cp Dist/libfreeimage.a ../../../../../target/classes/linux64/lib
+#cd ..
+#rm -r workdir
+
+#exit ${r1}
+
+
+
---
+++ src/main/scripts/win64/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ../../c++
+./configure --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32
+make
+
+make install
+
+r1=$?
+
+mkdir -p /src/target/classes/win64/lib
+mkdir -p /src/target/classes/win64/include
+
+exit ${r1}
