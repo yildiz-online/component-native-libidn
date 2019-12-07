6199be3274bd7bddec060f90612219e8ba0c29ef
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-07 16:05:15 +0100

    [WIP] Support libiconv.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -2,6 +2,7 @@
 
 #export $ LDFLAGS="-L/src/src/main/scripts/win64/libiconv/lib" CFLAGS="-I/src/src/main/scripts/win64/libiconv/include"
 
+autoreconf -f -i
 cd ../../c++
 ./configure --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32
 make
