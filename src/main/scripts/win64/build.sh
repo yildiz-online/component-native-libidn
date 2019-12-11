94ab4f378622e07ad2c99684abfbc8cf4883d2a9
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-11 11:55:20 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#export $ LDFLAGS="-L/src/src/main/scripts/win64/libiconv/lib" CFLAGS="-I/src/src/main/scripts/win64/libiconv/include"
+export $ LDFLAGS="-L/src/src/main/scripts/win64/libiconv/lib" CFLAGS="-I/src/src/main/scripts/win64/libiconv/include"
 
 cd ../..
 
