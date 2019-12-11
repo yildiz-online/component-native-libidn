9a8ef950906403002ff37026957fc8f7f479d3d6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-11 16:55:22 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -24,8 +24,7 @@ r1=$?
 
 mkdir -p /src/target/classes/win64/lib
 mkdir -p /src/target/classes/win64/include
-cp /usr/local/lib/libidn2.a /src/target/classes/win64/lib/libidn2.a
-cp /usr/local/lib/libidn2.dll.a /src/target/classes/win64/lib/libidn2.dll.a
+cp -r /usr/local/lib /src/target/classes/win64/
 cp /usr/local/bin/libidn2-0.dll /src/target/classes/win64/lib/libidn2-0.dll
 cp -r /usr/local/include /src/target/classes/win64/
 
