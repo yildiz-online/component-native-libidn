08d7e30039b7304a1781cccbe8ace790089db78b
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-11 11:20:49 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -2,18 +2,11 @@
 
 #export $ LDFLAGS="-L/src/src/main/scripts/win64/libiconv/lib" CFLAGS="-I/src/src/main/scripts/win64/libiconv/include"
 
-echo "Retrieving sources..."
 cd ../..
-
-echo "Removing source"
 
 rm -r c++
 
-echo "Curl"
-
 curl https://ftp.gnu.org/gnu/libidn/libidn2-2.3.0.tar.gz -o libidn2-2.3.0.tar.gz
-
-echo "untar"
 
 tar xvzf libidn2-2.3.0.tar.gz
 mv libidn2-2.3.0 c++
@@ -23,9 +16,13 @@ cd c++
 ./configure --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32
 make
 
+make install
+
 r1=$?
 
 mkdir -p /src/target/classes/win64/lib
 mkdir -p /src/target/classes/win64/include
+cd /src/target/classes/win64/lib
+ls -l
 
 exit ${r1}
